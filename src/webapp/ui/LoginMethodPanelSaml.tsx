2de61fec74d4a2311123bef97afedf5bd230ec79
gthomas2 <brudinie@gmail.com>
2025-03-05 16:46:49 +0100

    Force saml auth plugin to use same version of react as mnet. Fix missing lang string issue in login panel

--- src/webapp/ui/LoginMethodPanelSaml.tsx
+++ src/webapp/ui/LoginMethodPanelSaml.tsx
@@ -1,7 +1,7 @@
 import { PrimaryButton } from '@moodlenet/component-library'
 import { getCurrentLang } from '@moodlenet/core/i18n'
-import type { LocalSamlConfig } from '../../server/types.mjs'
 import type { FC } from 'react'
+import type { LocalSamlConfig } from '../../server/types.mjs'
 import { shell } from '../shell.mjs'
 
 let label = ''
@@ -11,7 +11,7 @@ async function loadConfig() {
   }
   const config: LocalSamlConfig = await shell.rpc.me('config')()
   const lang = getCurrentLang()
-  label = config.linkText[lang] ?? 'Log in using Saml!'
+  label = config?.linkText?.[lang] ?? 'Log in using Saml!'
 }
 await loadConfig()
 
